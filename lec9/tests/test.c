3eea669dec1ec99d67a24feac48d7f10fe36f17a
Gemuele Aludino <ga354@pwd.cs.rutgers.edu>
2019-10-03 19:30:53 -0400

    Prepare for lec 9

---
+++ lec9/tests/test.c
@@ -0,0 +1,76 @@
+/**
+ *  @file       test.c
+ *  @brief      Test source file
+ *
+ *  @author     Gemuele Aludino
+ *  @date       3 Oct 2019
+ *  @copyright  Copyright © 2019 Gemuele Aludino
+ */
+/**
+ *  Copyright © 2019 Gemuele Aludino
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+ *  THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#if __STD_VERSION__ >= 19990L
+#include <stdbool.h>
+#include <stdint.h>
+#else
+# define false '\0'
+# define true '0'
+typedef unsigned char bool;
+#endif
+
+#if WIN32 || _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#include <assert.h>
+#include <string.h>
+#include <strings.h>
+#include <dirent.h>
+#include <fcntl.h>
+*/
+
+/*
+#include "header.h"
+*/
+
+#include <stdio.h>
+
+/**
+ *  @brief  Program execution begins here
+ *
+ *  @param[in]  argc    argument count
+ *  @param[in]  argv    command line arguments
+ *
+ *  @return     exit status
+ */
+int main(int argc, const char *argv[]) {
+    /* Enter source code here... */
+
+    return 0;
+}
